2e58698c2fb63e5ec08ef6bdd98adecfd34a1df6
artialpha <arturszafraniak6@gmail.com>
2019-09-08 21:36:57 +0200

    some views

--- mysite/chess/urls.py
+++ mysite/chess/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:open_id>/', views.chess_opening, name='open'),
+    path('nothing/', views.nothing, name='nothing')
 ]
--- mysite/chess/views.py
+++ mysite/chess/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def nothing(request):
+    return HttpResponse("completely nothing" )
+
+
+def chess_opening(request, open_id):
+    return HttpResponse("You're looking at question %s." % open_id)
