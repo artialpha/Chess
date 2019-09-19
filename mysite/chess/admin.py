fe50417e9b29f3f5c436d8684647b573b57f7d39
artialpha <arturszafraniak6@gmail.com>
2019-09-19 20:21:36 +0200

    button in admin

--- mysite/chess/admin.py
+++ mysite/chess/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+
 from .models import *
+from django.urls import path
 # Register your models here.
 
-admin.site.register(ChessOpening)
+
 admin.site.register(Variant)
+
+
+@admin.register(ChessOpening)
+class ChessOpeningAdmin(admin.ModelAdmin):
+    change_list_template = "admin/chess_open_add.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('immortal/', self.add),
+        ]
+        return my_urls + urls
+
+    def add(self, request):
+        print("buahahuah")
+        return HttpResponseRedirect("../")
+
+
+
+
+
+
+
+
+
+
+
+
+
